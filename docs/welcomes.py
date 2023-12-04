868c5691dc7f538df3299cb827334942ce67c758
sky <adsky0309@gmail.com>
2023-12-04 12:49:19 +0900

    first commit

---
+++ docs/welcomes.py
@@ -0,0 +1,14 @@
+## 변수 선언 후 정의 시 고려점 (넣는 값이 문자인가 숫자인가)
+
+# 문자 출력
+helloworld = "Hello, world! SkY"  #문자형 변수
+print(helloworld)
+
+# 숫자 합산 출력
+numbers =3+5
+print(numbers)
+
+# 가정문
+if 5>2 : #묶음 기호인 :과 tab은 하나의 쌍
+    print("Five is greater than two")
+print("end")
