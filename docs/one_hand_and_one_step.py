81945d2cf7ecc1b6c6897dcafcfb552220e1c87e
sky <adsky0309@gmail.com>
2023-12-04 16:01:15 +0900

    quest_1

--- docs/one_hand_and_one_step.py
+++ docs/one_hand_and_one_step.py
@@ -1,65 +1,37 @@
 #방법1
 
-card1="1"
-card2=" "
-card3="2"
-card4=" "
-card5="3"
+c1="1"
+c2=" "
+c3="2"
+c4=" "
+c5="3"
 
-print(card1, card2, card3, card4, card5)
+print(c1,c2,c3,c4,c5)
 
-card1=" "
-card2="1"
-card3="2"
-card4=" "
-card5="3"
+c2=c1
+c1=" "
+print(c1,c2,c3,c4,c5)
 
-print(card1, card2, card3, card4, card5)
+c4=c3
+c3=" "
+print(c1,c2,c3,c4,c5)
 
-card1=" "
-card2="1"
-card3=" "
-card4="2"
-card5="3"
+c3=c5
+c5=" "
+print(c1,c2,c3,c4,c5)
 
-print(card1, card2, card3, card4, card5)
+c1=c3
+c3=" "
+print(c1,c2,c3,c4,c5)
 
-card1=" "
-card2="1"
-card3="3"
-card4="2"
-card5=" "
+c3=c2
+c2=" "
+print(c1,c2,c3,c4,c5)
 
-print(card1, card2, card3, card4, card5)
+c5=c3
+c3=" "
+print(c1,c2,c3,c4,c5)
 
-card1="3"
-card2="1"
-card3=" "
-card4="2"
-card5=" "
-
-print(card1, card2, card3, card4, card5)
-
-card1="3"
-card2="1"
-card3="2"
-card4=" "
-card5=" "
-
-print(card1, card2, card3, card4, card5)
-
-card1="3"
-card2=" "
-card3="2"
-card4="1"
-card5=" "
-
-print(card1, card2, card3, card4, card5)
-
-card1="3"
-card2=" "
-card3="2"
-card4=" "
-card5="1"
-
-print(card1, card2, card3, card4, card5)
+c3=c4
+c4=" "
+print(c1,c2,c3,c4,c5)
